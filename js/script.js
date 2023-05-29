4095d94f1fd6619107dce610570a2cd87595eb75
Marvel Faizal <88711805+zvell08@users.noreply.github.com>
2023-05-29 20:14:01 +0700

    up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var countDownDate = new Date("Jun 4, 2023 00:00:00").getTime();
+var countDownDate = new Date("Jun 17, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
     // Get the current date and time
     var now = new Date().getTime();
@@ -17,10 +17,10 @@ var x = setInterval(function() {
 
     // Output the result in an element with id="countdown"
     // Set the date we're counting down to
-    var countDownDate = new Date("Jun 4, 2023 00:00:00").getTime();
+    var countDownDate = new Date("Jun 17, 2023 00:00:00").getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    var x = setInterval(function () {
 
         // Get the current date and time
         var now = new Date().getTime();
